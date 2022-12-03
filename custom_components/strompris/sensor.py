b6d8d8fd56faa355399fe9d226cac5213dccedbf
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-12-03 19:21:17 +0100

    Alert test

--- custom_components/strompris/sensor.py
+++ custom_components/strompris/sensor.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from collections.abc import MutableMapping
 from datetime import datetime, timedelta
 import logging
-from typing import Any, List
+from typing import Any, List, Optional
 
 from strompris.common import getNorwayTime
 from strompris.const import SOURCE_HVAKOSTERSTROMMEN
@@ -44,13 +44,14 @@ from .const import DOMAIN, PRICE_ZONE, PRICE_ZONES
 
 _LOGGER = logging.getLogger(__name__)
 
+strompris: Strompris = None
 
 def getSone(selected_price_zone: str) -> int:
     return PRICE_ZONES.index(selected_price_zone) + 1
 
 
 def uidPrisSone(pris_sone: str) -> str:
-    return f"{DOMAIN.lower()}_pris_sone_{pris_sone}"
+    return f"{DOMAIN.lower()}_pris_sone_no{pris_sone}"
 
 
 async def async_setup_entry(
@@ -62,55 +63,44 @@ async def async_setup_entry(
     addable = []
 
     sone = hass.data[PRICE_ZONE]
-    entities = [StromPrisSensor(pris_sone_nummer=getSone(sone), pris_sone=sone)]
-    async_add_entities(entities, True)
-
-
-""""
-    for entity in entities:
-        foundId = entity_registry.async_get_entity_id(
-            "sensor", DOMAIN, entity.unique_id
-        )
-        if not foundId:
-            addable.append(entity)
-        else:
-            print("Skipping", entity.unique_id)
-
-    if len(addable) > 0:
-        async_add_entities(addable, True)
-"""
-
+    
+    # Declaring global in order to make it re-usable
+    global strompris
+    strompris = Strompris(source=SOURCE_HVAKOSTERSTROMMEN, zone=getSone(sone))
+    
+    
+    entities = [
+        StromprisSensor(strompris),
+        StromprisAlertSensor(strompris)
+        ]
+    async_add_entities(entities, True)    
 
 class StromSensor(SensorEntity):
-    _pris_sone_nummer: int
-    _pris_sone: str
     _attr_has_entity_name = True
     _attr_extra_state_attributes = {}
 
     strompris: Strompris
 
-    def __init__(self, pris_sone_nummer: int, pris_sone: str) -> None:
-        self._pris_sone_nummer = pris_sone_nummer
-        self._pris_sone = pris_sone
+    def __init__(self, strompris: Strompris) -> None:
         super().__init__()
-        self.strompris = Strompris(
-            source=SOURCE_HVAKOSTERSTROMMEN, zone=pris_sone_nummer
-        )
+        self.strompris = strompris
+        self._last_updated = None
+        
 
 
-class StromPrisSensor(StromSensor):
+class StromprisSensor(StromSensor):
     """Representation of a generic Strompris Sensor."""
 
     _price_end: datetime | None = None
 
-    def __init__(self, pris_sone_nummer: int, pris_sone: str) -> None:
-        super().__init__(pris_sone_nummer, pris_sone)
+    def __init__(self, strompris: Strompris) -> None:
+        super().__init__(strompris)
         self._last_updated = None
         self._attr_native_unit_of_measurement = "NOK/kWh"
         self._attr_device_class = SensorDeviceClass.MONETARY
         self._attr_state_class = SensorStateClass.TOTAL
-        self._attr_unique_id = uidPrisSone(pris_sone=pris_sone)
-        self._attr_name = f"Electricity price - {pris_sone}"
+        self._attr_unique_id = uidPrisSone(pris_sone=strompris.priceSource._price_zone)
+        self._attr_name = f"Electricity price - NO{strompris.priceSource._price_zone}"
         self._model = "Price Sensor"
 
     @property
@@ -127,7 +117,7 @@ class StromPrisSensor(StromSensor):
             self._attr_available = False
             return
 
-        current: Prising = await self.strompris.async_get_current_price()
+        current: Pris = await self.strompris.async_get_current_price()
         if not current:
             _LOGGER.error(
                 "Could not obtain current electricity pricing. Setting sensor available state to False"
@@ -155,7 +145,7 @@ class StromPrisSensor(StromSensor):
         await self.async_fetch_prices_for_tomorrow_with_throttle()
 
     @Throttle(timedelta(minutes=5))
-    async def async_fetch_prices_for_tomorrow_with_throttle(self) -> list[Prising]:
+    async def async_fetch_prices_for_tomorrow_with_throttle(self) -> list[Pris]:
         tomorrow = await self.strompris.async_get_prices_for_tomorrow()
         if tomorrow is None or len(tomorrow) == 0:
             print("Fikk ingen priser for i morgen..")
@@ -174,26 +164,145 @@ class StromPrisSensor(StromSensor):
         return tomorrow
 
 
-"""
-    async def settPrisingPaaSensor(self) -> None:
-        data = await self.transformer_til_graf()
-        statistics = []
-        for pris in self._pris_i_dag:
-            statistics.append(
-                StatisticData(start=pris.start, state=pris.NOK_kwh, sum=pris.NOK_kwh)
-            )
+# class StromprisAverageToday(StromSensor):
+#     """_summary_
+#     """
+#     
+#     @property
+#     def icon(self) -> str:
+#         """Icon of the entity."""
+#         return "mdi:cash"
 
-        statistic_id = f"sensor.{self._attr_unique_id}"  # f"{DOMAIN}:energy_{self._attr_unique_id}"
 
-        metadata = StatisticMetaData(
-            has_mean=False,
-            has_sum=True,
-            name=f"{self._pris_sone} - Electricity price",
-            source=DOMAIN,
-            statistic_id=statistic_id,
-            unit_of_measurement=self._attr_native_unit_of_measurement,
-        )
 
-        print(statistics)
-        async_add_external_statistics(self.hass, metadata, statistics)
-"""
+class StromprisAlertSensor(StromSensor):
+        
+    TYPE_INCREASE = "INCREASE"
+    TYPE_DECREASE = "DECREASE"
+    
+    def __init__(self, strompris: Strompris) -> None:
+        super().__init__(strompris)
+        self.__attr_friendly_name = "Electricity Price alert"
+        self._attr_unique_id = f"{uidPrisSone(pris_sone=strompris.priceSource._price_zone)}_ALERT"
+        self._attr_name = f"Electricity price - NO{strompris.priceSource._price_zone} ALERT"
+        
+    @property
+    def icon(self) -> str:
+        """Icon of the entity."""
+        return "mdi:flash-alert"
+
+    @Throttle(timedelta(minutes=15))
+    async def async_update(self) -> None:
+        """Update extreme price changes
+        """
+        tomorrow = await self.strompris.async_get_prices_for_tomorrow()
+        if tomorrow is None or len(tomorrow) == 0:
+            return
+        floor = await self.strompris.async_get_extreme_price_reductions(tomorrow)
+        roof = await self.strompris.async_get_extreme_price_increases(tomorrow)
+        
+        floors = self.__group(floor)
+        roofs = self.__group(roof)
+        
+        msgAttr = self.__compose_message(floors=floors, roofs=roofs)        
+        attr = {
+            "friendly_name": "Electricity Price alert",
+            "message": msgAttr["message"],
+            "tts": msgAttr["tts"]
+        }
+        self._attr_extra_state_attributes.update(attr)
+    
+    def __group(self, prices: List[Pris]) -> List[List[Pris]]:
+        grouped: List[List[Pris]] = []
+        
+        group: List[Pris] = []
+        for price in prices:
+            prev = group[-1]
+            if (prev is None):
+                group.append(prev)
+            else:
+                if (prev.slutt == price.start):
+                    group.append(price)
+                else:
+                    if (len(group) > 0):
+                        grouped.append(group)
+                    group = []
+        return grouped
+        
+    def __compose_message(self, floors: List[List[Pris]], roofs: List[List[Pris]]) -> dict[str, str]:
+        #groups: List[PriceDef] = []       
+        messages: List[str] = []
+        tts: List[str] = []
+        if (len(floors) > 0 and len(roofs) > 0):
+            messages.append("Strømprisen for i morgen vil variere kraftig.")
+            tts.append("Strømprisen for i morgen vil variere kraftig.")
+        elif (len(floors) > 0 and len(roofs) == 0):
+            messages.append("Strømprisen for i morgen vil falle en del.")
+            tts.append("Strømprisen for i morgen vil falle en del.")
+        elif (len(floors) == 0 and len(roofs) > 0):
+            messages.append("Strømprisen for i morgen vil stige en del.")
+            tts.append("Strømprisen for i morgen vil stige en del.")
+        
+        floor: Optional[List[Pris]] = None
+        if (len(floors) > 0):
+            floor = next(filter(lambda inner: inner != None and min(inner, key=lambda p: p.kwh) , floors))
+        roof: Optional[List[Pris]] = None
+        if (len(roofs) > 0):
+            roof = next(filter(lambda inner: inner != None and max(inner, key=lambda p: p.kwh) , roofs))
+        
+        if (floor != None and roof != None):
+            if (floor[-1].start < roof[-1].start):
+                # Start med floor
+                messages.append(self.__compose_message_price_change(floor, self.TYPE_DECREASE, False))
+                tts.append(self.__compose_message_price_change(floor, self.TYPE_DECREASE, True))
+            else:
+                messages.append(self.__compose_message_price_change(roof, self.TYPE_INCREASE, False))
+                tts.append(self.__compose_message_price_change(roof, self.TYPE_INCREASE, True))
+        elif (floor != None):
+            messages.append(self.__compose_message_price_change(floor, self.TYPE_DECREASE, False))
+            tts.append(self.__compose_message_price_change(floor, self.TYPE_DECREASE, True))
+        elif (roof != None):
+            messages.append(self.__compose_message_price_change(roof, self.TYPE_INCREASE, False))
+            tts.append(self.__compose_message_price_change(roof, self.TYPE_INCREASE, True))
+            
+        # Join arrays and assign to attr
+        message_str = ' '.join(messages)
+        tts_str = ' '.join(tts)
+        return {
+            "message": message_str,
+            "tts": tts_str
+        }
+            
+    
+    def __ore_or_nok(self, pris: float, tts: bool = False) -> str:
+        if (pris < 1.0):
+            return "Øre"
+        else:
+            if (tts == False):
+                return "Kroner"
+            else:
+                return "Kr"
+        
+    def __compose_message_price_change(self, prices: List[Pris], type: TYPE_INCREASE | TYPE_DECREASE, tts: bool = False):
+        start = prices[0]
+        
+        time = "Kl"
+        if (tts):
+            time = "Klokken"
+        
+        current_direction: str
+        end_direction: str
+        if (type == self.TYPE_DECREASE):
+            current_direction = "falle"
+            end_direction = "øker"
+        else:
+            current_direction = "øke"
+            end_direction = "faller"
+        
+        if (len(Pris) == 1):
+            return f"Fra {time} {start.start.hour} til {time} {start.slutt.hour} vil prisen {current_direction} til {start.total} {self.__ore_or_nok(start.total, tts)}, før den {end_direction} igjen."
+        else:
+            slutt = prices[-1]
+            return f"I perioden Kl {start.start.hour} til Kl {slutt.slutt.hour} vil prisen falle til {start.total} {self.__ore_or_nok(start.total, tts)}, for deretter å ende på {slutt.total} {self.__ore_or_nok(slutt.total, tts)} før den øker igjen."
+                    
+        
