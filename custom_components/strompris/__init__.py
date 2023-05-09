7f38b2a6f3a5196bca99e1b252cab5945c2f0fb5
Brage Skjønborg <brage.skjonborg@hotmail.no>
2023-05-09 22:55:51 +0200

    Update __init__.py

--- custom_components/strompris/__init__.py
+++ custom_components/strompris/__init__.py
@@ -22,7 +22,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # TODO 3. Store an API object for your platforms to access
     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+   for platform in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
 
     return True
 
